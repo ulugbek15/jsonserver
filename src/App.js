169189ffe3604aa13d2ddb8dea4c97043249f9ae
Ulugbek <61221916+ulugbek15@users.noreply.github.com>
2022-01-29 11:39:02 +0500

    commited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [update, setUpdate] = useState("");
+  const [modal, setModal] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [object, setObject] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/todos")
+      .then((res) => res.json())
+      .then((data) => setTodo(data));
+  }, [todo]);
+
+  const hendleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: new Date().getTime(),
+        content: inputValue,
+      }),
+    }).catch((e) => console.error(e));
+
+    e.target[0].value = null;
+  };
+
+  const deleteTodo = (id) => {
+    fetch(`http://localhost:3000/todos/${id}`, {
+      method: "DELETE",
+    }).catch((e) => console.error(e));
+  };
+
+  const updateTodo = (e) => {
+    e.preventDefault();
+
+    fetch(`http://localhost:3000/todos/${object.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: update,
+      }),
+    }).catch((e) => console.error(e));
+
+    e.target[0].value = null;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={(e) => hendleSubmit(e)}>
+        <input onChange={(e) => setInputValue(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+
+      {modal && (
+        <form onSubmit={(e) => updateTodo(e)}>
+          <input
+            placeholder={object.content}
+            onChange={(e) => setUpdate(e.target.value)}
+          />
+          <button>Update</button>
+          <button onClick={() => setModal(false)} >x</button>
+        </form>
+      )}
+
+      {todo.map((t) => {
+        return (
+            <li key={t.id}>
+              {t.content}
+              <button onClick={() => deleteTodo(t.id)}>X</button>
+              <button onClick={() => {
+                setModal(true)
+                setObject(t)
+              }}>Update</button>
+            </li>
+        );
+      })}
+    </>
   );
 }
 
